822b2d3b6e211b36a1cfef0900f7b51ab3f8e4f7
Kavya Paliwal <kpaliwal2002@gmail.com>
2023-07-18 22:11:26 +0530

    update

--- one.java
+++ one.java
@@ -1,5 +1,7 @@
 /*Find the Largest element in an array */
 
+/*OPTIMAL APPROACH / RECURSIVE APPROACH
+
 public class one {
     public static void main(String[] args) {
         int arr1[]={2,5,1,3,0};
@@ -16,3 +18,30 @@ public class one {
         return max;
     }
 }
+
+Time Complexity: O(N)
+Space Complexity: O(1)
+*/
+
+/*BRUTE FORCE
+ * 
+ * 
+import java.util.*;
+public class tuf {
+ 
+  public static void main(String args[]) {
+ 
+    int arr1[] =  {2,5,1,3,0};
+    System.out.println("The Largest element in the array is: " + sort(arr1));
+   
+    int arr2[] =  {8,10,5,7,9};
+    System.out.println("The Largest element in the array is: " + sort(arr2));
+  }
+  static int sort(int arr[]) {
+    Arrays.sort(arr);
+    return arr[arr.length - 1];
+  }
+}
+Time Complexity: O(N*log(N))
+Space Complexity: O(n)
+ */
--- two.java
+++ two.java
@@ -1,3 +1,27 @@
-public class two {
-    
+/*Find Second Smallest and Second Largest Element in an array */
+/*Print ‘-1’ in the event that either of them doesn’t exist. */
+
+import java.util.*;
+
+public class two{
+    static private void getElements(int[] arr,int n){
+        if(n==0||n==1){
+            System.out.print(-1);
+            System.out.print(" ");
+            System.out.print(-1);
+            System.out.print("\n");
+        }
+        Arrays.sort(arr);
+        int small = arr[1];
+        int large = arr[n-2];
+        System.out.println(small);
+        System.out.print(large);
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6};
+        int n = arr.length;
+        getElements(arr,n);
+    }
 }
