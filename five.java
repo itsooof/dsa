eda790c637b18d929bbe3a401a4f1d65b7d74244
Kavya Paliwal <kpaliwal2002@gmail.com>
2023-07-19 16:25:18 +0530

    update

--- five.java
+++ five.java
@@ -4,6 +4,7 @@ Given an integer array nums, rotate the array to the right by k steps,
 where k is non-negative.
  */
 
+ /*
 public class five {
 
     void rightRotate(int nums[], int k){
@@ -16,3 +17,23 @@ public class five {
         }
     }
 }    
+*/
+
+import java.util.*;
+
+class five{
+    static void leftRotate(int[] nums,int n){
+        int temp=nums[0];
+        for(int i=0;i<n-1;i++){
+            nums[i]=nums[i+1];
+        }
+        nums[n-1]=temp;
+        for(int i=0;i<n;i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
+
+/*Time Complexity: O(n), as we iterate through the array only once.
+
+Space Complexity: O(1) as no extra space is used */
