43a30e8c47a9f5488c299564112cd8e27e12d207
Kavya Paliwal <kpaliwal2002@gmail.com>
2023-07-21 09:32:32 +0530

    Update 008.java

--- 008.java
+++ 008.java
@@ -2,7 +2,7 @@ public class eight {
     int search(int arr[], int n, int num){
         int i;
         for(i=0;i<n;i++){
-            if(arr[i]=num)
+            if(arr[i]==num)
             return i;
         }
         return -1;
