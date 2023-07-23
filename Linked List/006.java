6796577bfc63dff43b7c2f5bb51a5bc5e4928a95
Kavya Paliwal <kpaliwal2002@gmail.com>
2023-07-24 01:58:23 +0530

    Update 006.java

---
+++ Linked List/006.java
@@ -0,0 +1,10 @@
+/* Add 1 to a number represented as linked list*/
+
+/*A number N is represented in Linked List such that each digit corresponds to a node in linked list. 
+You need to add 1 to it. */
+
+/*input: 9->9->9
+output: 1->0->0->0
+ */
+
+ 
