232e2801aa40d51abde2c3298799a24760d0d697
Kavya Paliwal <kpaliwal2002@gmail.com>
2023-07-26 22:37:51 +0530

    Update 002.java

--- Matrix/002.java
+++ Matrix/002.java
@@ -1,1 +1,23 @@
-/*799. Champagne Tower */
+/* 799. Champagne Tower */
+/*https://leetcode.com/problems/champagne-tower/ */
+
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        if(poured==0) return 0;
+        int rows=100;
+        double[][] tower= new double[rows+1][rows+1];
+        tower[0][0]=poured;
+
+        for(int row=0;row<=query_row;row++){
+            for(int cols=0;cols<=row;cols++){
+                double splitwine=(tower[row][cols]-1.0)/2.0;
+                if(splitwine>0){
+                    tower[row+1][cols]+=splitwine;
+                    tower[row+1][cols+1]+=splitwine;
+                }
+            }
+        }
+        return Math.min(1, tower[query_row][query_glass]);
+
+    }
+}
